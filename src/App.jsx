9cc594fb31f5cbf3a246079c9978dd4c1300f997
e.lapitskaya <e.lapitskaya@space307.com>
2022-01-23 02:37:18 +0300

    Create Home and About components.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Home from "./routes/Home/Home";
+
+function App() {
+  return (
+      <div className="app">
+          <h1 className="appName">Eda v lapkah</h1>
+          <Home />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/routes/About/About.jsx
@@ -0,0 +1,10 @@
+import './About.css';
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return <p>Инфромация о проекте</p>;
+    }
+}
+
+export default About;
